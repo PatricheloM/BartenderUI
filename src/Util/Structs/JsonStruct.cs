5b03bf3b7e8cb52227d9ce6259997393d39de6b9
Kun Patrik <kunpatrik00@mailbox.unideb.hu>
2022-09-08 23:30:40 +0200

    adding assembly info and project files and fixing a json EOF

--- src/Util/Structs/JsonStruct.cs
+++ src/Util/Structs/JsonStruct.cs
@@ -2,6 +2,6 @@
 {
     class JsonStruct
     {
-        public const string SettingsDefault = "{\n\t\"RedisConnection\": \"127.0.0.1:6379\",\n\t\"RedisPassword\": \"password\"\n}";
+        public const string SettingsDefault = "{\n\t\"RedisConnection\": \"127.0.0.1:6379\",\n\t\"RedisPassword\": \"password\"\n}\n";
     }
 }
